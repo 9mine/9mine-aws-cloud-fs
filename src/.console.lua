4938b870675c7dbbc598f9b5596722ada8e3b980
Oleksandr Dievri <o.dievri@gmail.com>
2021-02-03 17:04:08 +0200

    poll responses until aws command execution finished

--- src/.console.lua
+++ src/.console.lua
@@ -2,22 +2,23 @@
 
 connection:set_cmdchan(aws_cmdchan(connection, core_conf:get("cmdchan_path")))
 local tx_name = "9mine-aws-cloud-fs-ec2-conf.png"
-texture.download("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/AWS_Simple_Icons_AWS_Cloud.svg/1200px-AWS_Simple_Icons_AWS_Cloud.svg.png", true,
-tx_name, "9mine-aws-cloud-fs")
+texture.download(
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/AWS_Simple_Icons_AWS_Cloud.svg/1200px-AWS_Simple_Icons_AWS_Cloud.svg.png",
+    true, tx_name, "9mine-aws-cloud-fs")
 entity:set_properties({
-  visual = "cube",
-  textures = {tx_name, tx_name, tx_name, tx_name, tx_name, tx_name}
+    visual = "cube",
+    textures = {tx_name, tx_name, tx_name, tx_name, tx_name, tx_name}
 })
 entity:get_luaentity().on_punch = function(self, player)
     local p = self.object:get_pos()
     local pos = minetest.serialize(p)
     local request = ""
     minetest.show_formspec(player:get_player_name(), "aws:console",
-             table.concat({"formspec_version[4]", "size[13,13,false]",
-    "textarea[0.5,0.5;12.0,10;;;" .. minetest.formspec_escape(self.output) .. "]",
-    "field[0.5,10.5;12,1;input;;]", "field_close_on_enter[input;false]",
-    "button[10,11.6;2.5,0.9;send;send]",
-    "field[13,13;0,0;entity_pos;;" .. minetest.formspec_escape(pos) .. "]"}, ""))
+        table.concat({"formspec_version[4]", "size[13,13,false]",
+                      "textarea[0.5,0.5;12.0,10;;;" .. minetest.formspec_escape(self.output) .. "]",
+                      "field[0.5,10.5;12,1;input;;]", "field_close_on_enter[input;false]",
+                      "button[10,11.6;2.5,0.9;send;send]",
+                      "field[13,13;0,0;entity_pos;;" .. minetest.formspec_escape(pos) .. "]"}, ""))
 end
 
 local function aws_console(player, formname, fields)
@@ -31,17 +32,24 @@ local function aws_console(player, formname, fields)
         local cmdchan = connections:get_connection(player_name, lua_entity.addr):get_cmdchan()
         cmdchan:write(fields.input:gsub("^aws ", ""))
         minetest.show_formspec(player_name, "aws:console",
-        table.concat({"formspec_version[4]", "size[13,13,false]", "textarea[0.5,0.5;12.0,10;;;",
-                      minetest.formspec_escape("Please, wait for response"), "]", "field[0.5,10.5;12,1;input;;]",
-                      "field_close_on_enter[input;false]"}, ""))
+            table.concat({"formspec_version[4]", "size[13,13,false]", "textarea[0.5,0.5;12.0,10;;;",
+                          minetest.formspec_escape("Please, wait for response"), "]", "field[0.5,10.5;12,1;input;;]",
+                          "field_close_on_enter[input;false]"}, ""))
         minetest.after(3, function()
-            local response = cmdchan:read("/n/cmdchan/cmdchan_output")
-            lua_entity.output = fields.input .. ": \n" .. response .. "\n" .. lua_entity.output
-            minetest.show_formspec(player_name, "aws:console",
-                table.concat({"formspec_version[4]", "size[13,13,false]", "textarea[0.5,0.5;12.0,10;;;",
-                              minetest.formspec_escape(lua_entity.output), "]", "field[0.5,10.5;12,1;input;;]",
-                              "field_close_on_enter[input;false]", "button[10,11.6;2.5,0.9;send;send]",
-                              "field[13,13;0,0;entity_pos;;", minetest.formspec_escape(fields.entity_pos), "]"}, ""))
+            local function show_output()
+                local result, response = pcall(cmdchan.read, cmdchan, "/n/cmdchan/cmdchan_output")
+                if not result then
+                    minetest.after(3, show_output)
+                    return
+                end
+                lua_entity.output = fields.input .. ": \n" .. response .. "\n" .. lua_entity.output
+                minetest.show_formspec(player_name, "aws:console",
+                    table.concat({"formspec_version[4]", "size[13,13,false]", "textarea[0.5,0.5;12.0,10;;;",
+                                  minetest.formspec_escape(lua_entity.output), "]", "field[0.5,10.5;12,1;input;;]",
+                                  "field_close_on_enter[input;false]", "button[10,11.6;2.5,0.9;send;send]",
+                                  "field[13,13;0,0;entity_pos;;", minetest.formspec_escape(fields.entity_pos), "]"}, ""))
+            end
+            show_output()
         end)
     end
 end
